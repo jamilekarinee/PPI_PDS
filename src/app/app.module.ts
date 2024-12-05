0ca99423103cf8446c8a4e581a77b407cda4d413
jamilekarinee <139991955+jamilekarinee@users.noreply.github.com>
2024-12-05 08:27:48 -0300

    05 de dezembro de 2024

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TelaInicialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
